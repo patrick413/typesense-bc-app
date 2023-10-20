9ad26ad0ad3ca3c2c08618600bddcb2a20e69773
patrick413 <patrick@four13.co>
2023-10-20 08:46:51 +0800

    remove post

--- pages/api/channels/[cid].ts
+++ pages/api/channels/[cid].ts
@@ -32,19 +32,6 @@ export default async function channels(req: NextApiRequest, res: NextApiResponse
                 res.status(response?.status || 500).json({ message });
             }
             break;
-        
-        case 'POST':
-            try {
-                const { accessToken, storeHash } = await getSession(req);
-                const bigcommerce = bigcommerceClient(accessToken, storeHash);
-
-                const { data } = await bigcommerce.put(`/catalog/products/${pid}`, body);
-                res.status(200).json(data);
-            } catch (error) {
-                const { message, response } = error;
-                res.status(response?.status || 500).json({ message });
-            }
-            break;
         default:
             res.setHeader('Allow', ['GET', 'PUT']);
             res.status(405).end(`Method ${method} Not Allowed`);
